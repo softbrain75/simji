fd3e9055232b5e255bef17857a45cca1bc9328cc
softbrain <softbrain75@naver.com>
2026-08-23 15:26:31 +0900

    Show member login code as text

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'simji-pwa-v55';
+const CACHE_NAME = 'simji-pwa-v56';
 const APP_SHELL = [
   '/',
   '/index.html',
   '/styles.css?v=19',
   '/detail-edit.css?v=11',
   '/media.css?v=19',
-  '/app.js?v=48',
+  '/app.js?v=49',
   '/config.js',
   '/manifest.webmanifest?v=4',
   '/icons/simji-heart.svg?v=1',
